e19948e9c5c8ea0aae075a22e1e27577b4a99dd5
Robin Millette <robin@millette.info>
2017-05-31 15:55:47 -0400

    Show rows as html.

--- pages/index.js
+++ pages/index.js
@@ -4,13 +4,23 @@ import Pouchdb from 'pouchdb-browser'
 
 export default class MyPage extends React.Component {
   static async getInitialProps (oy) {
-    if (oy && oy.req) {
-      return { rows: false }
-    }
+    // server side
+    if (oy && oy.req) { return { rows: false } }
 
     const db = new Pouchdb('mooya')
     // await db.post({ now: new Date().toISOString(), one: 'thing' })
     return db.allDocs()
+  }
+
+  rows () {
+    if (this.props.rows) {
+      return this.props.rows.map((row) => <dl>
+        <dt>{row.id}</dt>
+        <dd>{row.value.rev}</dd>
+      </dl>)
+    }
+
+    return <p>Server side...</p>
   }
 
   render () {
@@ -20,10 +30,7 @@ export default class MyPage extends React.Component {
           <a>ROWS</a>
         </Link>
       </h2>
-      <pre>
-        {JSON.stringify(this.props.rows, null, '  ')}
-      </pre>
+      {this.rows()}
     </div>
   }
 }
-
