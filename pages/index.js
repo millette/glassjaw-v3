e62668117c73b7a84e73be802bb90ffa246f1d08
Robin Millette <robin@millette.info>
2017-05-31 15:37:48 -0400

    Use pouchdb.

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,29 @@
-export default () => (
-  <div>Welcome to next.js!</div>
-)
+import React from 'react'
+import Link from 'next/link'
+import Pouchdb from 'pouchdb-browser'
+
+export default class MyPage extends React.Component {
+  static async getInitialProps (oy) {
+    if (oy && oy.req) {
+      return { rows: false }
+    }
+
+    const db = new Pouchdb('mooya')
+    // await db.post({ now: new Date().toISOString(), one: 'thing' })
+    return db.allDocs()
+  }
+
+  render () {
+    return <div>
+      <h2>
+        <Link href='/'>
+          <a>ROWS</a>
+        </Link>
+      </h2>
+      <pre>
+        {JSON.stringify(this.props.rows, null, '  ')}
+      </pre>
+    </div>
+  }
+}
+
