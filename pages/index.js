6021b7f30628bd3c5e2b5ea4d298c1d6b2e4c3c0
Robin Millette <robin@millette.info>
2017-06-02 17:12:58 -0400

    Use pouchdb plugins.

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,38 @@
 import React from 'react'
 import Link from 'next/link'
 import Header from '../components/progress'
-import Pouchdb from 'pouchdb-browser'
+import Pouchdb from 'pouchdb-core'
+
+Pouchdb.plugin(require('pouchdb-adapter-http'))
+
+const cfg = {
+  db: false,
+  remote: 'http://localhost:5993/gj-v3',
+  local: 'mooya'
+}
 
 export default class MyPage extends React.Component {
   static async getInitialProps (oy) {
     // server side
-    if (oy && oy.req) { return { rows: false } }
-
-    const db = new Pouchdb('mooya')
-    // await db.post({ now: new Date().toISOString(), one: 'thing' })
-    return db.allDocs()
+    if (oy && oy.req) {
+      if (!cfg.db) { cfg.db = new Pouchdb(cfg.remote) }
+    } else {
+      if (!cfg.db) {
+        Pouchdb
+          .plugin(require('pouchdb-adapter-idb'))
+          .plugin(require('pouchdb-adapter-websql'))
+          .plugin(require('pouchdb-replication'))
+        cfg.db = new Pouchdb(cfg.local)
+      }
+      cfg.db.remove('job', '1-967a00dff5e02add41819138abb3284d')
+      Pouchdb.sync(cfg.remote, cfg.local)
+    }
+    return cfg.db.allDocs()
   }
 
   rows () {
     if (this.props.rows) {
-      return this.props.rows.map((row) => <div>
+      return this.props.rows.map((row) => <div key={row.id}>
         <p>The content!</p>
         <dl>
           <dt>{row.id}</dt>
