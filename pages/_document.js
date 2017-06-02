96e6824de9ba00b0205dffae6b920387882011b9
Robin Millette <robin@millette.info>
2017-06-02 17:13:20 -0400

    Add utf-8 charset.

--- pages/_document.js
+++ pages/_document.js
@@ -10,6 +10,9 @@ export default class MyDocument extends Document {
 
   render () {
     return <html lang='fr' manifest='/the.appcache'>
+      <head>
+        <meta charSet='utf-8' />
+      </head>
       <body>
         <Main />
         <NextScript />
