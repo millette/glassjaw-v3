6e3e332c634d0a2696d6bbc71c0fef103f3f854b
Robin Millette <robin@millette.info>
2017-06-04 17:20:15 -0400

    Talk to couchdb directly.

--- pages/index.js
+++ pages/index.js
@@ -2,30 +2,42 @@ import React from 'react'
 import Link from 'next/link'
 import Header from '../components/progress'
 import Pouchdb from 'pouchdb-core'
+// import utils from '../utils'
 
 Pouchdb.plugin(require('pouchdb-adapter-http'))
 
 const cfg = {
   db: false,
-  remote: 'http://localhost:3000/db/gj-v3',
+  remote: 'http://localhost:5993/gj-v3',
+  // remoteDb: 'gj-v3',
   local: 'mooya'
 }
 
 export default class MyPage extends React.Component {
   static async getInitialProps (oy) {
-    // server side
+    // cfg.remote = ['http://localhost:5993', cfg.remoteDb].join('/')
+    // cfg.remote = [utils.dbUrl(oy), cfg.remoteDb].join('/')
+    // console.log('REMOTE:', cfg.remote)
+    // if (!cfg.remote) { cfg.remote = [utils.dbUrl(oy), cfg.remoteDb].join('/') }
     if (oy && oy.req) {
+      // server side
       if (!cfg.db) { cfg.db = new Pouchdb(cfg.remote) }
     } else {
+      // client side
       if (!cfg.db) {
         Pouchdb
           .plugin(require('pouchdb-adapter-idb'))
           .plugin(require('pouchdb-adapter-websql'))
           .plugin(require('pouchdb-replication'))
+          .sync(cfg.remote, cfg.local, { live: true, retry: true })
+          .on('active', () => console.log('ACTIVE'))
+          .on('change', (info) => console.log('CHANGE', info))
+          .on('complete', (info) => console.log('COMPLETE', info))
+          .on('paused', (err) => console.log('PAUSED', err))
+          .on('denied', (err) => console.log('DENIED', err))
+          .on('error', (err) => console.log('ERROR', err))
         cfg.db = new Pouchdb(cfg.local)
       }
-      cfg.db.remove('job', '1-967a00dff5e02add41819138abb3284d')
-      Pouchdb.sync(cfg.remote, cfg.local)
     }
     return cfg.db.allDocs()
   }
